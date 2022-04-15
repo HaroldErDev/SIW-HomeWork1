b16e43abc0ee1f700d105070a26c616788e854dd
HaroldDev <tinaritiziano.dev@gmail.com>
2022-04-15 18:49:47 +0200

    Added Class "Indirizzo"

---
+++ siw-homework-1/src/it/uniroma3/siw/model/Indirizzo.java
@@ -0,0 +1,78 @@
+package it.uniroma3.siw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/* Politiche Fetch: sono state lasciate di default */
+/* Politiche Cascade: non sono stati applicati eventi a cascata */
+
+@Entity
+public class Indirizzo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String via;
+	
+	private int numeroCivico;
+	
+	private String comune;
+	
+	private String cap;
+	
+	private String provincia;
+	
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public int getNumeroCivico() {
+		return numeroCivico;
+	}
+
+	public void setNumeroCivico(int numeroCivico) {
+		this.numeroCivico = numeroCivico;
+	}
+
+	public String getComune() {
+		return comune;
+	}
+
+	public void setComune(String comune) {
+		this.comune = comune;
+	}
+
+	public String getCap() {
+		return cap;
+	}
+
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+}
--- siw-homework-1/src/it/uniroma3/siw/model/Societa.java
+++ siw-homework-1/src/it/uniroma3/siw/model/Societa.java
@@ -1,12 +1,13 @@
 package it.uniroma3.siw.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /* Politiche Fetch: sono state lasciate di default */
-/* Politiche Cascade: non sono stati applicati eventi a cascata */
 
 @Entity
 public class Societa {
@@ -17,17 +18,12 @@ public class Societa {
 	
 	private String ragioneSociale;
 	
-	private String via;
-	
-	private int numeroCivico;
-	
-	private String comune;
-	
-	private String cap;
-	
-	private String provincia;
-	
 	private String telefono;
+	
+	/* Ad una societa' e' relativo un indirizzo; quindi quando creo la societa' creo a sua volta anche l'indirizzo.
+	 * In modo analogo se cancello la societa', quest'ultima non fara' piu' parte di quell'indirizzo e quindi va cancellato anche lui. */
+	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private Indirizzo indirizzo;
 	
 
 	public Long getId() {
@@ -46,52 +42,20 @@ public class Societa {
 		this.ragioneSociale = ragioneSociale;
 	}
 
-	public String getVia() {
-		return via;
-	}
-
-	public void setVia(String via) {
-		this.via = via;
-	}
-
-	public int getNumeroCivico() {
-		return numeroCivico;
-	}
-
-	public void setNumeroCivico(int numeroCivico) {
-		this.numeroCivico = numeroCivico;
-	}
-
-	public String getComune() {
-		return comune;
-	}
-
-	public void setComune(String comune) {
-		this.comune = comune;
-	}
-
-	public String getCap() {
-		return cap;
-	}
-
-	public void setCap(String cap) {
-		this.cap = cap;
-	}
-
-	public String getProvincia() {
-		return provincia;
-	}
-
-	public void setProvincia(String provincia) {
-		this.provincia = provincia;
-	}
-
 	public String getTelefono() {
 		return telefono;
 	}
 
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
+	}
+
+	public Indirizzo getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(Indirizzo indirizzo) {
+		this.indirizzo = indirizzo;
 	}
 	
 }
